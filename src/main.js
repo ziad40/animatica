5d03278c8cc6dfdc4671a2f7309e929445db03d2
ziad40 <zizoabuelkher@gmail.com>
2025-09-18 15:35:53 +0300

    start animatica project by creating 3d scene

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './style.css';
+import { initThreeScene } from './components/ThreeScene.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initThreeScene(); // Call your 3D initialization here
+});
+
