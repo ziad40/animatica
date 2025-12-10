cfe4c610e893af58751035719c2d8aed07c86b9e
ziad40 <zizoabuelkher@gmail.com>
2025-12-10 14:56:31 +0200

    fix 3d screen height in mobile

--- src/components/layout/Interactive.jsx
+++ src/components/layout/Interactive.jsx
@@ -19,7 +19,7 @@ const Interactive = ({ problem, isMobile, threeDMode, currentProblemId, setCurre
 
       {/* 3D (mount only after entering 3D the 1st time) */}
       {hasOpened3D && (
-        <div className={`${threeDMode ? "block" : "hidden"} w-full h-full flex-1 min-w-0`} >
+        <div className={`${threeDMode ? "block" : "hidden"} w-full h-screen flex-1 min-w-0`} >
           <ThreeDInteractive problem={problem} threeDMode={threeDMode}/>
         </div>
       )}
